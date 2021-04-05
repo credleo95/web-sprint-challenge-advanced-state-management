3acd733fa7573e2a77ce52871fde3d297d5d1702
Omarius Credle <omariuscredle@omariuss-air.home>
2021-04-05 02:11:18 -0400

    created empty reducers. Starting on action creators and axios call.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,30 @@
 import axios from 'axios';
+import thunk from 'redux-thunk';
+
+export const START_FETCH = "START_FETCH"
+export const FETCH_SUCCESS = "FETCH_SUCCESS"
+export const FETCH_FAIL= "FETCH_FAIL"
+export const ADD_SMURF= "ADD_SMURF"
+export const ADD_ERROR_VALUE= "ADD_ERROR_VALUE"
+
+
+export const addSmurf = () => {
+
+}
+
+export const fetchSmurf = (dispatch) => {
+dispatch({type:START_FETCH}); 
+axios.get()
+.then(res => { 
+    console.log(res.data)
+    dispatch({type:FETCH_SUCCESS, payload: res.data})
+
+})
+.catch(error => {
+    console.log('Cannot load smurfs')
+    dispatch({type:FETCH_FAIL, payload: error})
+})
+}
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
